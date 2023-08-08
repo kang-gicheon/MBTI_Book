8ea64d1895ea4560ebd4a4fdb7fe517c24f0b898
kimyegun123 <kimyegun123@gmail.com>
2023-08-08 09:23:21 +0900

    메인 폴더 확정
    쓸모없는 폴더 정리 및 폴더명 변경

--- kicheon/src/pages/Login.js
+++
@@ -1,7 +0,0 @@
-import "./Login.css";
-
-const Login = () => {
-  return <div>로그인 페이지 예정</div>;
-};
-
-export default Login;
--- kicheon/src/pages/Register.js
+++
@@ -1,7 +0,0 @@
-import "./Register.css";
-
-const Register = () => {
-  return <div>회원가입페이지 예정</div>;
-};
-
-export default Register;
